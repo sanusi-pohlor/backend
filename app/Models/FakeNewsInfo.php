e558adfaa2eaabfe69bef9a407b4166904a9e816
sanusi-pohlor <134247007+sanusi-pohlor@users.noreply.github.com>
2024-01-12 17:31:41 +0700

    120167.1

--- app/Models/FakeNewsInfo.php
+++ app/Models/FakeNewsInfo.php
@@ -10,6 +10,6 @@ class FakeNewsInfo extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'fn_info_nameid', 'fn_info_head','fn_info_province', 'fn_info_content', 'fn_info_source', 'fn_info_num_mem', 'fn_info_more', 'fn_info_link', 'fn_info_dmy', 'fn_info_image', 'fn_info_vdo','fn_info_status','created_at'
+        'id',  'fn_info_nameid', 'fn_info_head','fn_info_province', 'fn_info_content', 'fn_info_source', 'fn_info_num_mem', 'fn_info_more', 'fn_info_link', 'fn_info_dmy', 'fn_info_image', 'fn_info_vdo','fn_info_status','created_at'
     ];
 }
